790656ac90f119e11fb0c2d53777236566d62b95
storemalt <storemaltinside@gmail.com>
2019-05-19 20:47:11 +0800

    feat(form): error validation message on text box

--- app/Helpers/StringHelper.php
+++ app/Helpers/StringHelper.php
@@ -10,7 +10,7 @@ class StringHelper
      * @param string $word
      * @return string unique string
      */
-    public static function longestUniqueString(string $word): string
+    public function longestUniqueString(string $word): string
     {
         return count_chars($word, 3);
     }
