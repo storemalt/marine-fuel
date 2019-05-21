9d6539ed35cfbe897ea61ef47da3665660ff0004
storemalt <storemaltinside@gmail.com>
2019-05-21 20:41:44 +0800

    feat(test): added unique longest string test to check if result string is sorted

--- tests/Unit/StringHelperLongestUniqueStringTest.php
+++ tests/Unit/StringHelperLongestUniqueStringTest.php
@@ -8,7 +8,7 @@ use Tests\TestCase;
 class StringHelperLongestUniqueStringTest extends TestCase
 {
     /**
-     * A basic unit test example.
+     * Checks if a non unique data will result to a unique string
      *
      * @return void
      */
@@ -22,6 +22,11 @@ class StringHelperLongestUniqueStringTest extends TestCase
         $this->assertEquals(4, strlen($answer));
     }
 
+    /**
+     * Checks empty data results in an empty result data
+     *
+     * @return void
+     */
     public function testEmptyParamsExpectResultEmptyString()
     {
         $word = '';
@@ -31,12 +36,31 @@ class StringHelperLongestUniqueStringTest extends TestCase
         $this->assertEmpty($answer);
     }
 
+    /**
+     * Checks if a unique data will result to the same string
+     *
+     * @return void
+     */
     public function testUniqueDataExpectsUniqueResultString()
     {
         $word = 'abcd';
         $stringHelper = new StringHelper();
         $answer = $stringHelper->longestUniqueString($word);
 
+        $this->assertSame($word, $answer);
+    }
+
+    /**
+     * Checks if a unique reversed data will result to a sorted string
+     *
+     * @return void
+     */
+    public function testReversedUniqueDataExpectsUniqueResultStringSorted()
+    {
+        $word = 'dcba';
+        $stringHelper = new StringHelper();
+        $answer = $stringHelper->longestUniqueString($word);
+        $answer = strrev($answer);
         $this->assertSame($word, $answer);
     }
 }
